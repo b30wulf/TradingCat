6f17c904c96e8c1ae0cfc7f89a0fb5f3b32fe2d5
mcaner <caner_izci@hotmail.com>
2019-10-10 01:45:59 +0300

    add repository and update readme

---
+++ TradingCat/source/Core/DataAccess/Core.DataAccess/Entities/AuditedEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core.DataAccess.Entities
+{
+    public abstract class AuditedEntity : BaseEntity
+    {
+        public bool IsDeleted { get; set; }
+        public int? DeleterId { get; set; }
+        public DateTime? DeletedDateTime { get; set; }
+        public int? ModifiedId { get; set; }
+        public DateTime? ModifiedDateTime { get; set; }
+        public int? CreatorId { get; set; }
+        public DateTime? CreatedDateTime { get; set; }
+    }
+}
---
+++ TradingCat/source/Core/DataAccess/Core.DataAccess/Repositories/IRepositoryAsync.cs
@@ -0,0 +1,19 @@
+﻿using Core.DataAccess.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Repositories
+{
+    public interface IRepositoryAsync<T> where T : IEntity, new()
+    {
+        Task<T> AddAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<IEnumerable<T>> ListAsync();
+        Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate);
+        Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        Task<T> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate);
+    }
+}
