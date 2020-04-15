ffc0224e377a2d563287f063cfd9d2842e7c14cc
mcanerizci <mcaner.izci@gmail.com>
2020-04-15 23:08:35 +0300

    Estimate service infrastructure added

---
+++ TradingCat/source/Services/Estimate/Estimate.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Estimate.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ TradingCat/source/Services/Estimate/Estimate.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Estimate.Domain
+{
+    public class Class1
+    {
+    }
+}
